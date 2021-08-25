20c1f61c79d1ce7d2d7f36d9bd7c7d6641696db9
gaoshiming <gaoshiming@zdns.cn>
2021-08-25 06:14:08 +0000

    fix(nodejs): Fix babel transform and slash version.

--- nodejs/packages/scripts/src/config/babel-transform.js
+++ nodejs/packages/scripts/src/config/babel-transform.js
@@ -1,4 +1,6 @@
-const babelJest = require('babel-jest')
+const babelJestMd = require('babel-jest');
+const babelJest = babelJestMd.__esModule ? babelJestMd.default : babelJestMd;
+
 
 module.exports = babelJest.createTransformer({
   presets: [require.resolve('./babelrc')],
