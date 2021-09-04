0a5ccb1b18ff5f343e9b93c3c8d318ea36fd0a8b
gsmlg <me@gsmlg.org>
2021-09-05 00:14:57 +0800

    feat(flutter): Add router to flutter.

--- flutter/gsmlg/lib/main.dart
+++ flutter/gsmlg/lib/main.dart
@@ -22,7 +22,11 @@ class GSMLG extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.red,
       ),
-      home: MyHomePage(title: 'GSMLG'),
+      initialRoute:"/",
+      routes:{
+        "new_page":(context) => RouterTestRoute(),
+        "/":(context) =>  MyHomePage(title: 'GSMLG'),
+      } 
     );
   }
 }
@@ -101,6 +105,12 @@ class _MyHomePageState extends State<MyHomePage> {
               '-- Harvey Dent',
               style: Theme.of(context).textTheme.headline5,
             ),
+            TextButton(
+              child: Text("open new route"),
+              onPressed: () {
+                Navigator.pushNamed(context, "new_page");
+              },
+            ),
           ],
         ),
       ),
@@ -109,6 +119,64 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+
+
+class RouterTestRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        child: Text("打开提示页"),
+        onPressed: () async {
+          var result = await Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) {
+                return TipRoute(
+                  text: "我是提示xxxx",
+                );
+              },
+            ),
+          );
+          print("路由返回值: $result");
+        },
+
+      ),
+    );
+  }
+}
+
+class TipRoute extends StatelessWidget {
+  TipRoute({
+    Key? key,
+    @required this.text = "", // 接收一个text参数
+  }) : super(key: key);
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("提示"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(18),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Text(text),
+              ElevatedButton(
+                onPressed: () => Navigator.pop(context, "我是返回值"),
+                child: Text("返回"),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
