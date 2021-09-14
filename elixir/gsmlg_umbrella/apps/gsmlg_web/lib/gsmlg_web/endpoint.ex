f1448d24f189e3ff50ee571af3e5f209a12153f7
gaoshiming <gaoshiming@zdns.cn>
2021-09-14 16:41:59 +0000

    feat(elixir:gsmlg): Add node channel in GSMLGWeb app.

--- elixir/gsmlg_umbrella/apps/gsmlg_web/lib/gsmlg_web/endpoint.ex
+++ elixir/gsmlg_umbrella/apps/gsmlg_web/lib/gsmlg_web/endpoint.ex
@@ -12,6 +12,10 @@ defmodule GSMLGWeb.Endpoint do
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
 
+  socket "/socket", GSMLGWeb.UserSocket,
+    websocket: true,
+    longpoll: false
+
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
