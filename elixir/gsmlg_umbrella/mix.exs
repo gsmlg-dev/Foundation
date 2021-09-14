dd4c949d4bfc49e47265cc24ef20948ec54e50e8
gaoshiming <gaoshiming@zdns.cn>
2021-09-14 15:33:39 +0000

    feat(elixir:gsmlg): Add distillery to create OTP release.

--- elixir/gsmlg_umbrella/mix.exs
+++ elixir/gsmlg_umbrella/mix.exs
@@ -24,7 +24,9 @@ defmodule GSMLG.Umbrella.MixProject do
   # Dependencies listed here are available only for this project
   # and cannot be accessed from applications inside the apps/ folder.
   defp deps do
-    []
+    [
+      {:distillery, "~> 2.0", runtime: false},
+    ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
