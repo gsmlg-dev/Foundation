5ac165c4fcd1278e233aca8e231e5fd75fc1c240
gaoshiming <gaoshiming@zdns.cn>
2021-09-20 08:05:14 +0000

    feat(elixir:gsmlg): Add node channel.

--- elixir/gsmlg_umbrella/apps/gsmlg_web/lib/gsmlg_web/channels/node_channel.ex
+++ elixir/gsmlg_umbrella/apps/gsmlg_web/lib/gsmlg_web/channels/node_channel.ex
@@ -1,25 +1,29 @@
 defmodule GSMLGWeb.NodeChannel do
   use GSMLGWeb, :channel
 
-  @impl true
   def join("node:lobby", payload, socket) do
     if authorized?(payload) do
+      send(self(), {:after_join, payload})
       {:ok, socket}
     else
       {:error, %{reason: "unauthorized"}}
     end
   end
 
+  def handle_info({:after_join, _msg}, socket) do
+    state = GSMLG.Node.Self.get_state()
+    push socket, "node_state", %{name: state.self, isAlive: state.alive?, nodes: GSMLG.Node.Others.get_nodes(), node_list: Node.list}
+    {:noreply, socket}
+  end
+
   # Channels can be used in a request/response fashion
   # by sending replies to requests from the client
-  @impl true
   def handle_in("ping", payload, socket) do
     {:reply, {:ok, payload}, socket}
   end
 
   # It is also common to receive messages from the client and
   # broadcast to everyone in the current topic (node:lobby).
-  @impl true
   def handle_in("shout", payload, socket) do
     broadcast socket, "shout", payload
     {:noreply, socket}
