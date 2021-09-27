5e8fd3750f2096b9b980c90b0ac42d1f7f06f430
gaoshiming <gaoshiming@zdns.cn>
2021-09-27 12:42:34 +0000

    feat(elixir:gsmlg): Update phoenix to 1.6.0 production.

--- elixir/gsmlg_umbrella/apps/gsmlg_web/mix.exs
+++ elixir/gsmlg_umbrella/apps/gsmlg_web/mix.exs
@@ -37,7 +37,7 @@ defmodule GSMLGWeb.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.6.0-rc.0", override: true},
+      {:phoenix, "~> 1.6.0", override: true},
       {:phoenix_ecto, "~> 4.4"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
