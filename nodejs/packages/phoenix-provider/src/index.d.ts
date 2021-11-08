619c4d23ea339aaae2c7a9c9508ed77d3d3b1e04
gaoshiming <gaoshiming@zdns.cn>
2021-11-08 07:46:03 +0000

    feat(nodejs:phoenix-provider): Add type definition.

---
+++ nodejs/packages/phoenix-provider/src/index.d.ts
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import {Socket, Presence, Channel} from 'phoenix';
+
+const PhoenixContext = React.createContext({});
+
+PhoenixContext.displayName = 'Phoenix';
+
+export interface ProviderArgs {
+    url: string;
+    params: object;
+    children: JSX.Element
+};
+
+export function Provider(arg : ProviderArgs) : JSX.Element;
+
+export const Consumer: PhoenixContext.Consumer;
+
+export function useSocket() : Socket | null;
+
+export function useChannel(topic: string, params: object) : Channel | null;
+
+export function usePresence(topic: string) : Presence | null;
+
