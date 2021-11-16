8405253cca97b83f6d87afe409fc59d246efb588
gaoshiming <gaoshiming@zdns.cn>
2021-11-16 10:24:48 +0000

    fix(nodejs:varian): Fix build.

--- nodejs/packages/varian/rollup.config.js
+++ nodejs/packages/varian/rollup.config.js
@@ -1,5 +1,3 @@
-import babel from "rollup-plugin-babel";
-import uglify from "rollup-plugin-uglify";
 import replace from "rollup-plugin-replace";
 import commonjs from "rollup-plugin-commonjs";
 import resolve from "rollup-plugin-node-resolve";
@@ -14,10 +12,6 @@ const config = {
   external: [
   ],
   plugins: [
-    babel({
-      exclude: "node_modules/**",
-      plugins: ["external-helpers"]
-    }),
     resolve(),
     commonjs({
       include: /node_modules/
@@ -27,9 +21,5 @@ const config = {
     })
   ]
 };
-
-if (process.env.NODE_ENV === "production") {
-  config.plugins.push(uglify());
-}
 
 export default config;
--- nodejs/packages/varian/tools/test-preprocessor.js
+++ nodejs/packages/varian/tools/test-preprocessor.js
@@ -4,16 +4,23 @@ const babelJest = require('babel-jest');
 module.exports = {
   process: (src, path, ...rest) => {
     // CoffeeScript files can be .coffee, .litcoffee, or .coffee.md
+    console.log(path);
+
     if (coffee.helpers.isCoffee(path)) {
-      return coffee.compile(src, {
+      const compiledCode = coffee.compile(src, {
         filename: path,
         literate: coffee.helpers.isLiterate(path),
         inlineMap: true,
         bare: true,
         transpile: {
-          presets: ['jest', '@babel/env']
-        }
+          presets: [
+            'jest',
+            '@babel/env'
+          ],
+        },
       });
+
+      return compiledCode;
     }
     if (!/node_modules/.test(path)) {
       return babelJest.process(src, path, ...rest);
