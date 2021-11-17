546bcddbb9bb4f9d961fc0dbda0388f72d7067ef
gaoshiming <gaoshiming@zdns.cn>
2021-11-17 04:09:48 +0000

    fix(nodejs:eslint-config-gsmlg): Fix rules no-unused-private-class-members.

--- nodejs/packages/eslint-config/es6/stylistic.js
+++ nodejs/packages/eslint-config/es6/stylistic.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['./non-rules-config.js', 'prettier'],
   rules: {
-    'no-unused-private-class-members': 'off',
+    'no-unused-private-class-members': 'warn',
     'arrow-body-style': 'off',
     'object-shorthand': ['error', 'properties'], // methods are optional so you can specify a name if you want
     'prefer-arrow-callback': [
