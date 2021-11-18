5329b9c7caf5b6635f7820cc6e00f797a5d0a286
gaoshiming <gaoshiming@zdns.cn>
2021-11-18 15:06:39 +0000

    refactor(nodejs:website): Refactor vultr-latency.

--- nodejs/packages/website/pages/tools/vultr-latency.tsx
+++ nodejs/packages/website/pages/tools/vultr-latency.tsx
@@ -13,7 +13,84 @@ import Grid from '@mui/material/Grid';
 import Layout from 'components/Layout';
 import NodeCard from '@gsmlg/react/dist/mui/Vultr/NodeCard';
 
-import hosts from 'vultrHosts';
+const Hosts = [
+  {
+    host: 'fra-de-ping.vultr.com',
+    name: '法兰克福',
+  },
+  {
+    host: 'ams-nl-ping.vultr.com',
+    name: '阿姆斯特丹',
+  },
+  {
+    host: 'par-fr-ping.vultr.com',
+    name: '巴黎',
+  },
+  {
+    host: 'lon-gb-ping.vultr.com',
+    name: '伦敦',
+  },
+  {
+    host: 'sgp-ping.vultr.com',
+    name: '新加坡',
+  },
+  {
+    host: 'hnd-jp-ping.vultr.com',
+    name: '东京',
+  },
+  {
+    host: 'nj-us-ping.vultr.com',
+    name: '新泽西',
+  },
+  {
+    host: 'il-us-ping.vultr.com',
+    name: '芝加哥',
+  },
+  {
+    host: 'ga-us-ping.vultr.com',
+    name: '亚特兰大',
+  },
+  {
+    host: 'wa-us-ping.vultr.com',
+    name: '西雅图',
+  },
+  {
+    host: 'fl-us-ping.vultr.com',
+    name: '迈阿密',
+  },
+  {
+    host: 'tx-us-ping.vultr.com',
+    name: '达拉斯',
+  },
+  {
+    host: 'sjo-ca-us-ping.vultr.com',
+    name: '硅谷',
+  },
+  {
+    host: 'lax-ca-us-ping.vultr.com',
+    name: '洛杉矶',
+  },
+  {
+    host: 'syd-au-ping.vultr.com',
+    name: '悉尼',
+  },
+];
+
+const vultrHosts = Hosts.map(
+  ({name, host}) => ({
+    id: host.replace('-ping.vultr.com', ''),
+    ping: [],
+    pending: false,
+    name,
+    host,
+    times: 0,
+    lost: 0,
+    delay: -1,
+    minDelay: -1,
+    maxDelay: -1,
+    averageDelay: -1,
+  }),
+);
 
 const StyledGrid = styled(Grid)(({
   theme
@@ -23,15 +100,14 @@ const StyledGrid = styled(Grid)(({
 
 
 function VultrNetworks() {
-
-  const [networks, setNetworks] = useState(hosts);
+  const [networks, setNetworks] = useState(vultrHosts);
 
   useEffect(() => {
     let isStopped: boolean = false;
     const run = async () => {
       out:
       while (true) {
-        for (let h of hosts) {
+        for (let h of vultrHosts) {
           if (isStopped) break out;
           const {host, id} = h;
           let newNetworks;
