41a72f66cc6e437729bd6e2006587e894690ad86
gsmlg <me@gsmlg.org>
2021-11-24 17:59:16 +0800

    fix(flutter:mirror): Fix test script.

--- flutter/mirror/test/widget_test.dart
+++ flutter/mirror/test/widget_test.dart
@@ -12,22 +12,22 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mirror/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Mirror initialize test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     WidgetsFlutterBinding.ensureInitialized();
     final cameras = await availableCameras();
     await tester.pumpWidget(Mirror(cameras: cameras));
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
+    expect(find.text('Mirror'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    // await tester.tap(find.byIcon(Icons.add));
+    // await tester.pump();
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // expect(find.text('0'), findsNothing);
+    // expect(find.text('1'), findsOneWidget);
   });
 }
