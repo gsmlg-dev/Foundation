f2b2408be01460e27a729b5a289f1dcbdc984f67
gaoshiming <gaoshiming@zdns.cn>
2021-12-11 07:26:18 +0000

    feat(nodjs:@gsmlg/visx): Init @visx chart library.

---
+++ nodejs/packages/visx/.eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: './node_modules/@gsmlg/scripts/eslint.js',
+  rules: {
+    '@babel/new-cap': 'off',
+  },
+  ignorePatterns: ['**/*.stories.ts', '**/*.stories.tsx'],
+};
