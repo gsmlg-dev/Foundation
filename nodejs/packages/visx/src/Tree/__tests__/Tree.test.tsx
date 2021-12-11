52c4d18eca67eec891060fd4989623131af57311
gaoshiming <gaoshiming@zdns.cn>
2021-12-11 16:15:16 +0000

    fix(nodejs:@gsmlg/visx): Add test to visx Tree.

--- nodejs/packages/visx/src/Tree/__tests__/Tree.test.tsx
+++ nodejs/packages/visx/src/Tree/__tests__/Tree.test.tsx
@@ -1,7 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import * as React from 'react';
-// import { render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import { Tree } from '../Tree';
+import data from '../data.sample';
 
-it('should have test', () => {
-  expect(true).toBeTruthy();
+it('<Tree /> should be render correctly', () => {
+  render(<Tree width={100} height={100} data={data} />);
+  expect(screen.getByText(/gsmlg/)).toBeTruthy();
 });
---
+++ nodejs/packages/visx/src/Tree/index.js
@@ -0,0 +1,5 @@
+import { hierarchy } from '@visx/hierarchy';
+import { Tree } from './Tree';
+
+export default Tree;
+export { hierarchy };
