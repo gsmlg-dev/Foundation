d44f0000ac16fd7cdead45cb594e14684235c524
gaoshiming <gaoshiming@zdns.cn>
2021-12-16 13:03:42 +0000

    fix(nodejs:@gsmlg/react): Fix lint.

--- nodejs/packages/react/src/components/Xiangqi/DndContext.tsx
+++ nodejs/packages/react/src/components/Xiangqi/DndContext.tsx
@@ -4,7 +4,7 @@
  *
  */
 
-import React, { useState, useEffect } from 'react';
+import * as React from 'react';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { TouchBackend } from 'react-dnd-touch-backend';
 import { DndProvider } from 'react-dnd';
