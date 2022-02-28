226d9298ffe342432cb9b8baa19dc846d795caf5
Jonathan Gao <gsmlg.com@gmail.com>
2022-02-28 17:52:26 +0800

    feat(golang:gsmlg-cli): Add golang gsmlg-cli module.

---
+++ golang/gsmlg-cli/cmd/blog.go
@@ -0,0 +1,82 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+    "encoding/json"
+	"fmt"
+    "io/ioutil"
+    "log"
+    "net/http"
+
+	"github.com/spf13/cobra"
+)
+
+type Blog struct {
+    Id  int  `json:"id"`
+    Slug    string  `json:"slug"`
+    Title   string  `json:"title"`
+    Date    string  `json:"date"`
+    Author  string  `json:"author"`
+    Content string  `json:"content"`
+}
+
+type BlogResponse struct {
+    Data    []Blog  `json:"data"`
+}
+
+// blogCmd represents the blog command
+var blogCmd = &cobra.Command{
+	Use:   "blog",
+	Short: "List blog",
+	Long: `Print blog info in terminal. Format:
+
+    Support plain text and json format.
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+        resp, err := http.Get("https://gsmlg.org/api/blogs")
+        if err != nil {
+            log.Fatalln(err)
+        }
+
+        body, err := ioutil.ReadAll(resp.Body)
+        if err != nil {
+            log.Fatalln(err)
+        }
+
+        var br BlogResponse
+        if err := json.Unmarshal(body, &br); err != nil {
+            log.Fatalln(err)
+        }
+
+        var maxLen int = 0
+        for _, b := range br.Data {
+            if l := len(b.Slug); l > maxLen {
+                maxLen = l
+            }
+        }
+
+        pf := fmt.Sprintf("%%-3d\t%%-%ds\t%%s\n", maxLen)
+
+        // fmt.Printf("%+v\n", b.Data)
+        for _, b := range br.Data {
+            fmt.Printf(pf, b.Id, b.Slug, b.Title)
+        }
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(blogCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// blogCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// blogCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ golang/gsmlg-cli/cmd/root.go
@@ -0,0 +1,84 @@
+/*
+Copyright © 2022 Jonathan Gao gsmlg.com@gmail.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "gsmlg-cli",
+	Short: "A command line tool for my private affair.",
+	Long:  `A command line tool for my private affair.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gsmlg/cli.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		// Search config in home directory with name ".cli" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigType("yaml")
+		viper.SetConfigName(".gsmlg/cli")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+}
