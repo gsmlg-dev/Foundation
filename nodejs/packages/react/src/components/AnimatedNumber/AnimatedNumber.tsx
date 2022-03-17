037bd7a26f15bec9f7ed56025183066edc674ef1
Jonathan Gao <gsmlg.com@gmail.com>
2022-03-17 11:45:39 +0800

    feat(nodejs:@gsmlg/react): Add AnimatedNumber.

---
+++ nodejs/packages/react/src/components/AnimatedNumber/AnimatedNumber.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+interface AnimatedNumberProps {
+  duration: number;
+  value: number;
+  formatValue: (v: number) => string;
+}
+
+const AnimatedNumber: React.FC<AnimatedNumberProps> = ({ duration = 1000, value, formatValue = (v) => `${v}`}) => {
+  const [animatedValue, setAnimatedValue] = React.useState(0);
+  const [animateOpt, setAnimateOpt] = React.useState({ duration, toValue: 0 });
+  React.useEffect(() => {
+    setAnimateOpt({ duration, toValue: value });
+  }, [duration, value]);
+  React.useEffect(() => {
+    let tick = -1;
+    const startTime : number = +new Date();
+    const endTime : number = new Date().valueOf() + duration;
+    const startValue = animatedValue;
+    const endValue = animateOpt.toValue;
+    const runAnimation = () => {
+      let t = +new Date();
+      if (t >= endTime) {
+        setAnimatedValue(endValue);
+        return;
+      }
+      const remained = endTime - t;
+      const percent = remained / (endTime - startTime);
+      const val = startValue + ((endValue - startValue) * percent);
+      setAnimatedValue(val);
+      tick = requestAnimationFrame(runAnimation);
+    };
+    runAnimation();
+    return () => cancelAnimationFrame(tick);
+  }, [animateOpt]);
+  return (
+    <span>
+      {formatValue(animatedValue)}
+    </span>
+  );
+};
+
+export default AnimatedNumber;
---
+++ nodejs/packages/react/src/components/AnimatedNumber/index.tsx
@@ -0,0 +1,3 @@
+import AnimatedNumber from './AnimatedNumber';
+
+export default AnimatedNumber;
