dc655135ec4cf065f4230e608b55c136919b7a1b
Jonathan Gao <gsmlg.com@gmail.com>
2022-04-13 14:49:18 +0800

    feat(nodejs:@gsmlg/scripts): Add eslint-config-next.

--- nodejs/packages/scripts/src/config/eslintrc.js
+++ nodejs/packages/scripts/src/config/eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
     require.resolve('eslint-config-gsmlg/jest'),
     ifAnyDep('react', require.resolve('eslint-config-gsmlg/jsx-a11y')),
     ifAnyDep('react', require.resolve('eslint-config-gsmlg/react')),
+    ifAnyDep('next', require.resolve('eslint-config-next')),
   ].filter(Boolean),
   rules: {},
 }
