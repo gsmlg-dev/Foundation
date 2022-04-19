24519032a58da2eb421a260de6c4577184ac3d59
Jonathan Gao <gsmlg.com@gmail.com>
2022-04-20 00:11:23 +0800

    feat(nodejs:@gsmlg/lit): Update storybook theme.

---
+++ nodejs/packages/lit/.storybook/theme.js
@@ -0,0 +1,7 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  brandTitle: 'GSMLG.DEV',
+  brandUrl: 'https://gsmlg.org',
+  brandImage: 'https://gsmlg.github.io/svg/gsmlg-dev.svg',
+});
