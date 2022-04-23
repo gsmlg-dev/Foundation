8e1de7e586b5940561174bd45fcd069c6387ae24
Jonathan Gao <gsmlg.com@gmail.com>
2022-04-23 12:41:24 +0800

    chore: Add unit test.

--- nodejs/packages/lit/src/__tests__/remark-element.js
+++
@@ -1,5 +0,0 @@
-describe('remark-element', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(true);
-  });
-});
---
+++ nodejs/packages/lit/src/__tests__/remark-element.ts
@@ -0,0 +1,7 @@
+import '../remark-element';
+
+describe('remark-element', () => {
+  it('Expect custom element <remark-element> is defined', () => {
+    expect(customElements.get('remark-element')).not.toEqual(undefined);
+  });
+});
