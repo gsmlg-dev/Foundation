8073f02deb2910aff0b2e37ff43f89fd13a10ecb
Jonathan Gao <gsmlg.com@gmail.com>
2022-04-24 23:45:41 +0800

    feat(nodejs:@gsmlg/website): Update blogs and blog fetch script.

--- nodejs/packages/website/scripts/fetch_blog_data.js
+++ nodejs/packages/website/scripts/fetch_blog_data.js
@@ -4,11 +4,18 @@ const axios = require('axios').default;
 
 const DATA_FILE = path.join(__dirname, '..', 'data', 'blogs.json');
 
+const TOKEN = process.env.GSMLG_TOKEN;
+
 const main = async () => {
-  const response = await axios.get('https://gsmlg.org/api/blogs', { responseType: 'json' });
-  
+  const response = await axios.get('https://gsmlg.org/api/blogs', {
+    responseType: 'json',
+    headers: {
+      authorization: `Bearer ${TOKEN}`
+    },
+  });
+
   const blogList = response.data.data;
-  
+
   const content = JSON.stringify(blogList, null, 2);
 
   await fs.writeFile(DATA_FILE, content);
