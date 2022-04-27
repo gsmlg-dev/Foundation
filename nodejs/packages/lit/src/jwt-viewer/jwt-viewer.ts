8b77bcd83b7b3d85731772f76e8e9e38a9751a85
Jonathan Gao <gsmlg.com@gmail.com>
2022-04-27 12:30:37 +0800

    fix: Beatify jwt-viewer.

--- nodejs/packages/lit/src/jwt-viewer/jwt-viewer.ts
+++ nodejs/packages/lit/src/jwt-viewer/jwt-viewer.ts
@@ -3,6 +3,15 @@ import { customElement, property } from 'lit/decorators.js';
 
 import { parseJWT } from '@gsmlg/utils';
 
+type TokenType = {
+  header: {
+    typ: string;
+    alg: string;
+  };
+  payload: object;
+  signatrue: string;
+};
+
 @customElement('jwt-viewer')
 export class JwtViewer extends LitElement {
   static override styles = css`
@@ -11,15 +20,75 @@ export class JwtViewer extends LitElement {
       height: inherit;
       font-size: inherit;
     }
+    .row {
+      display: flex;
+      flex-direction: row;
+      justify-content: flex-start;
+      align-items: flex-start;
+    }
+    .row > label,
+    .row > div {
+      margin: 0.3rem 2rem;
+    }
+    .row > label {
+      width: 6rem;
+      text-align: right;
+      color: cadetblue;
+    }
+    .row > label::after {
+      display: inline-block;
+      content: ':';
+      color: hotpink;
+    }
+    .column {
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      align-items: flex-start;
+    }
+    .value {
+      color: cornflowerblue;
+    }
   `;
 
   @property({ type: String, reflect: true, attribute: true })
   token: string = '';
 
   override render() {
-    const p = parseJWT(this.token);
+    const p: TokenType = parseJWT(this.token);
     return html`
-      <pre>${JSON.stringify(p, null, 4)}</pre>
+      <div class="column">
+        <div class="row">
+          <label>Header</label>
+          <div class="column">
+            <div class="row">
+              <label>Algorithm</label>
+              <div class="value">${p.header.alg}</div>
+            </div>
+            <div class="row">
+              <label>Type</label>
+              <div class="value">${p.header.typ}</div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <label>Payload</label>
+          <div class="column">
+            ${Object.keys(p.payload).map(
+              (k) => html`
+                <div class="row">
+                  <label>${k}</label>
+                  <div class="value">${p.payload[k]}</div>
+                </div>
+              `,
+            )}
+          </div>
+        </div>
+        <div class="row">
+          <label>Signatrue</label>
+          <div class="value">${p.signatrue}</div>
+        </div>
+      </div>
     `;
   }
 }
