6ddfef16da88d5990070c07bd5a38e77c4865acb
Jonathan Gao <gsmlg.com@gmail.com>
2022-07-07 17:59:22 +0800

    fix(nodejs:@gsmlg/utils): Fix security issue with Cryptography API.

--- nodejs/packages/utils/src/uuid.js
+++ nodejs/packages/utils/src/uuid.js
@@ -1,21 +1,7 @@
 /**
  * UUID generator
- * Algorithms from rfc4122
- * Version v4
+ * Use Web Cryptography API
  */
-
-const createUUID = (function maker(uuidRegEx, uuidReplacer) {
-  return function uuid() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
-      .replace(uuidRegEx, uuidReplacer)
-      .toUpperCase();
-  };
-})(/[xy]/g, (c) => {
-  const r = (Math.random() * 16) | 0;
-  const v = c === 'x' ? r : (r & 3) | 8;
-  return v.toString(16);
-});
-
-export const uuid = createUUID();
+export const uuid = () => crypto.randomUUID();
 
 export default uuid;
