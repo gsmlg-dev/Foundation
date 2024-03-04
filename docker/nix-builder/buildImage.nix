c0c99afbfec7ec052d3f7b31d41a96a98ea00534
Jonathan Gao <gsmlg.com@gmail.com>
2024-03-04 16:16:38 +0800

    fix: Fix.

---
+++ docker/nix-builder/buildImage.nix
@@ -0,0 +1,10 @@
+{ pkgs ? import <nixpkgs> { }
+, pkgsLinux ? import <nixpkgs> { system = "x86_64-linux"; }
+}:
+
+pkgs.dockerTools.buildImage {
+  name = "hello-docker";
+  config = {
+    Cmd = [ "${pkgs.hello}/bin/hello" ];
+  };
+}
